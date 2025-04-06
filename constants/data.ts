b7d60a295a3598e537f6290dad1d20379a5f95fb
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-04-06 13:33:10 +0100

    rigorous debugging 😳

--- constants/data.ts
+++ constants/data.ts
@@ -29,17 +29,13 @@ const filters = {
   type: ["photo", "illustration", "vector"],
 
   colors: [
-    "grayscale",
-    "transparent",
     "red",
     "orange",
     "yellow",
     "green",
     "turquoise",
     "blue",
-    "lilac",
     "pink",
-    "white",
     "gray",
     "black",
     "brown",
